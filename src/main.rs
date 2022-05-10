8235bbcfd632c838b17ed645ec34fae73a710011
Shadow <suraj@ghishadow.com>
2022-05-10 13:23:46 +0530

    update ra to 2022-05-09

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ impl LapcePlugin for State {
         );
         if !PathBuf::from(&file_name).exists() {
             let url = format!(
-                "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-03-21/{}.gz",
+                "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-05-09/{}.gz",
                 file_name
             );
             let gz_file = PathBuf::from(file_name.clone() + ".gz");
